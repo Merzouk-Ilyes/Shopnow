bf1b4af27dae4d481d28119df6a7582d6be1ef5a
Merzouk-ilyes <i.merzouk@esi-sba.dz>
2022-02-17 21:22:52 +0100

    adding tailwindcss && setting folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @ts-ignore
+import Home from "./components/home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// import Cart from "./components/cart/Cart";
+// import Login from "./components/auth/Login"
+// import Register from "./components/auth/Register"
+// import Forget from "./components/auth/Forget"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="cart" element={<Cart />} /> */}
+          {/* <Route path="auth">
+            <Route path="" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="forget" element={<Forget />} />
+          </Route> */}
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "3rem" }}>
+                <p>There's nothing here , Leave me alone !</p>
+              </main>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
