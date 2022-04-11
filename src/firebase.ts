9d4523e78453665ddcf5f8cc09ec410e306bfff0
Merzouk-ilyes <i.merzouk@esi-sba.dz>
2022-04-12 00:12:38 +0100

    added firebase.ts :)

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAlHl8KQ5x1ERVyJME6KanyLOAouXTqLmU",
+  authDomain: "shopnow-b468c.firebaseapp.com",
+  projectId: "shopnow-b468c",
+  storageBucket: "shopnow-b468c.appspot.com",
+  messagingSenderId: "919427142667",
+  appId: "1:919427142667:web:0ccd10e3cf9695a460e30e"
+};
+
+
+
+// const app = initializeApp(firebaseConfig)
+// export const db = getFirestore(app);
